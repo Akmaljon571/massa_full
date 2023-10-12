a20732125aa5cc290965c6a833ab873575375f2d
Akmaljon571 <akmaljon079@gmail.com>
2023-10-12 16:57:32 +0300

    start: Bismillah

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header, Main, Footer } from './components'
+import './App.scss';
+function App() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    )
+} 
+
+export default App;
